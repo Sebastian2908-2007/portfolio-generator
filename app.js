1a63ec8e32d6995373430419b8d96b2f4dd4dfe5
sebastian <sebastian bowen>
2021-03-15 21:42:07 -0600

    save commit

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
+/*const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++) {
       console.log(profileDataArr[i]);
     }
-  }; printProfileData(profileDataArgs);
+    console.log('================');
+
+    //is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  }; printProfileData(profileDataArgs);*/
+
+  const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = (name, github) => {
+    return `
+   <!DOCTYPE html>
+   <html lang="en>
+   <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-compatible" content="ie=edge">
+   <title>Portfolio Demo</title>
+   </head>
+
+   <body>
+   <h1>${name}</h1>
+   <h2><a href="https://github.com/${github}">Github</a></h2>
+   </body>
+   </html>
+    `;
+}; 
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if (err) throw err;
+    console.log('portfolio complete! Check out index.html to see the output!')
+});
