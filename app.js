1da3037687098f0b6bfcfffcd136292ee19f3d43
sebastian <sebastian bowen>
2021-03-17 15:57:02 -0600

    inquirer working

--- app.js
+++ app.js
@@ -9,13 +9,25 @@
     //is the same as this...
     profileDataArr.forEach(profileItem => console.log(profileItem));
   }; printProfileData(profileDataArgs);*/
-  const fs = require('fs');
+
+  const inquirer = require('inquirer');
+
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'what is your name?'
+      }
+    ])
+    .then(answers => console.log(answers));
+  
+ /* const fs = require('fs');
   const generatePage = require('./src/page-template.js');
-  const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+ 
+  const pageHTML = generatePage(name, github)
 
-
-fs.writeFile('index.html', generatePage(name,github), err => {
+fs.writeFile('index.html', pageHTML, err => {
     if (err) throw err;
     console.log('portfolio complete! Check out index.html to see the output!')
-});
+});*/
