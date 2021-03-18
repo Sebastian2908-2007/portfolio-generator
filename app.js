58d83db5132430608bf145b4ba4f103dbc414df9
sebastian <sebastian bowen>
2021-03-18 00:05:35 -0600

    feature/questions issue complete

--- app.js
+++ app.js
@@ -41,9 +41,22 @@
        }
       },
       {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
         type: 'input',
         name: 'about',
-        message: 'provide some information about yourself:'
+        message: 'provide some information about yourself:',
+        when: ({confirmAbout}) => {
+          if (confirmAbout) {
+            return true;
+          }else{
+            return false;
+          }
+        }
       }
     ]);
   }
